8c31a8a7a9db13532dc83697f2f92bd0afe8c56a
MinsukJi-NOAA <minsuk.ji@noaa.gov>
2021-04-19 12:07:04 -0400

    Test 1-1

--- job_status_check.py
+++ job_status_check.py
@@ -55,13 +55,10 @@ def check_ec2(url, request, myid):
       in_progress.append(x["id"])
 
   while True: 
-    time.sleep(1)
+    time.sleep(20)
     done = []
     for cid, response in workflows.items():
-      #print(in_progress)
-      #print("cid:", cid)
       data = json.loads(response.read().decode())["jobs"]
-      #print(data)
       start_status = next(x["status"] for x in data if x["name"]=="Start runners")
       stop_status = next(x["status"] for x in data if x["name"]=="Stop runners")
       if start_status == "completed" and stop_status == "completed":
